3d8daa4373249e71cb50977e453dcc44c1ee0b34
dekapd99 <dekaprimatio@gmail.com>
2022-05-30 22:20:39 +0700

    Initial Commit

---
+++ InterCoV-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterCoV-19
+//
+//  Created by Deka Primatio on 30/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
